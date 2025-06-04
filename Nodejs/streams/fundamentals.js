eac49de813b70a2d7b707e98677ff656f18bf66a
Vitor Renan <129013548+Vitor-de-Almeida@users.noreply.github.com>
2025-06-04 17:54:22 +0000

    Daily Progress at Study

---
+++ Nodejs/streams/fundamentals.js
@@ -0,0 +1,36 @@
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+    index = 1
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            }else{
+                const buf = Buffer.from(String(i))
+                this.push(buf)
+            }
+        }, 1000);
+    }
+}
+
+class ReverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback){
+        const transformedNumber = Number(chunk.toString())*-1
+        callback(null, Buffer.from(String(transformedNumber)))
+
+    }
+}
+
+class MultilplayByTenStream extends Writable {
+     _write(chunk, encoding, callback){
+        console.log(Number(chunk.toString())*10)
+        callback()
+    }
+}
+
+new OneToHundredStream()
+.pipe(new ReverseNumberStream())
+.pipe(new MultilplayByTenStream())
---
+++ Nodejs/streams/stream-http.server.js
@@ -0,0 +1,7 @@
+import http from 'node;http'
+
+const server = http.createServer((request,response) => {
+
+})
+
+server.listen(3334)
