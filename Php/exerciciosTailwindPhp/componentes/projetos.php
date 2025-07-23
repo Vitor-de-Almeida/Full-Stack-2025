c9e9ce8315d2781440213022a5ffa7bd09ec4466
Vitor <vitorrenanalmeida@gmail.com>
2025-07-23 18:30:37 -0300

    Nearly done

--- Php/exerciciosTailwindPhp/componentes/projetos.php
+++ Php/exerciciosTailwindPhp/componentes/projetos.php
@@ -1,13 +1,40 @@
 <?php 
 $projetos = [
     [
-        "título" => "Meu portifolio",
-        "finalizado" => false,
-        "ano" => 2021,
-        "descricao" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam rem, ut perferendis neque vitae amet! Porro officiis assumenda nemo ad nobis modi quo optio fuga illum esse deleniti inventore, sint hic reiciendis laborum est odit possimus voluptatibus itaque? Hic laborum sit error mollitia. Cum quisquam incidunt ad voluptate numquam blanditiis!",
+        "título" => "React",
+        "finalizado" => true,
+        "ano" => 2024,
+        "descricao" => "Comecei a estudar React em 2024, e foi a biblioteca mais top que conheci no front-end. A forma como ela organiza os componentes e facilita a criação de interfaces modernas mudou totalmente minha maneira de programar.",
         "stack" => ["PHP", "HTML", "CSS", "JS"],
-        "alt" => "",
-        "img" => ""
+        "alt" => "React Logo",
+        "img" => "img/react.png"
+    ],
+    [
+        "título" => "PHP",
+        "finalizado" => true,
+        "ano" => 2025,
+        "descricao" => "Comecei a programar com PHP em 2025. Foi minha porta de entrada para o desenvolvimento back-end e me ajudou a entender a lógica por trás dos servidores, requisições e manipulação de dados. Simples, poderoso e amplamente usado.",
+        "stack" => ["PHP", "MySQL", "HTML", "CSS"],
+        "alt" => "PHP Logo",
+        "img" => "img/php.png"
+    ],
+    [
+        "título" => "Tailwind CSS",
+        "finalizado" => true,
+        "ano" => 2024,
+        "descricao" => "Conheci o Tailwind CSS em 2024 e ele revolucionou meu jeito de escrever estilos. Com utilitários diretos na marcação, consegui agilidade e padronização visual em meus projetos de forma muito mais simples e eficiente.",
+        "stack" => ["PHP", "Tailwind", "JS", "React"],
+        "alt" => "Tailwind CSS Logo",
+        "img" => "img/tailwind.png"
+    ],
+    [
+        "título" => "C#",
+        "finalizado" => false,
+        "ano" => 2025,
+        "descricao" => "Atualmente estou estudando C#. Estou explorando sua sintaxe robusta, orientação a objetos e potencial para aplicações desktop, web e APIs com .NET. Ainda estou no início, mas já percebo o quanto essa linguagem é poderosa e versátil.",
+        "stack" => ["C#", ".NET", "OOP", "RUST"],
+        "alt" => "C# Logo",
+        "img" => "img/csharp.png"
     ]
 ];
 ?>
@@ -16,15 +43,15 @@ $projetos = [
 
 <div class="bg-slate-800 rounded-lg p-3 flex items-center">
     <div class= "w-1/5 flex items-center justify-center">
-        <img src="<?= $projeto['img']?>" class="h-10" alt="<?= $projeto['alt']?>">
+        <img src="<?= $projeto['img']?>" class="h-24 w-24" alt="<?= $projeto['alt']?>">
     </div>
                 <div class="w-4/5 space-y-3">
                     <div class="flex gap-3 justify-between">
                         <h3 class="font-semibold text-xl">
-                            <?php if ($projeto['finalizado']):?> ✅ <?php endif;?>
+                            <?=$projeto['finalizado'] ? '✅' : '⛔'?>
                             <?= $projeto['título']?>
                             <?php if ($projeto['finalizado']):?>
-                                <span class="text-xs text-gray-400 opacity-50 italic">(finalizado em 2024)</span>
+                                <span class="text-xs text-gray-400 opacity-50 italic">(finalizado em <?=$projeto['ano']?>)</span>
                             <?php else: ?>
                                 <span class="text-sm text-gray-400 opacity-50 italic">(Em Desenvolvimento)</span>
                             <?php endif;?>
@@ -41,7 +68,7 @@ $projetos = [
                         </div>
                     </div>
                     <p class="leading-6">
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus rerum nemo tempora voluptas sit repudiandae pariatur, aliquam autem eos excepturi quia facilis debitis magnam qui culpa aliquid quasi et enim cum non eum ullam deserunt iusto! Ratione debitis incidunt, nulla optio nobis fugit possimus magni quasi aliquam, placeat quidem architecto.
+                        <?=$projeto["descricao"]?>
                     </p>
                  </div>
 </div>
--- Php/exerciciosTailwindPhp/index.php
+++ Php/exerciciosTailwindPhp/index.php
@@ -7,80 +7,20 @@
     <link rel="stylesheet" href="src/output.css">
 </head>
 <body class="bg-slate-900 text-gray-200">
-    <header class="mx-auto max-w-screen-lg px-3 py-6 flex items-center justify-between">
-        <div class="font-bold text-xl text-cyan-600">
-            Meu portfolio ...
-        </div>
-        <div class="">
-            <ul class="flex gap-x-3 font-medium">
-                <li><a href="#projetos" class="hover:underline">Projetos</a></li>
-                <li><a href="" class="hover:underline">Github</a></li>
-                <li><a href="" class="hover:underline">Linkedin</a></li>
-                <li><a href="" class="hover:underline">Twitter</a></li>
-            </ul>
-        </div>
-    </header>
+    <?php include('./componentes/header.php'); ?>
     <main class="mx-auto max-w-screen-lg min-h-20 px-3 py-6">
-        <section class="flex gap-x-3 border-b-2">
-            <div class="w-2/3">
-                <h1 class="text-3xl font-bold">
-                    Oi, meu nome é Vitor.
-                </h1>
-                <p class="text-xl leading-6 mt-6">
-                    Falando um pouco sobre mim, sou dev... programo em react e tailwindcss.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, voluptates!
-                </p>
-                <ul class="flex gap-x-3 mt-3">
-                    <li><a href="#">
-                        <img class="h-8 hover:animate-bounce" src="img/twitter.png" alt="Twitter Icon">
-                    </a></li>
-                    <li><a href="#">
-                        <img class="h-8 hover:animate-bounce" src="img/facebook.png" alt="Facebook Icon">
-                    </a></li>
-                    <li><a href="#">
-                        <img class="h-8 hover:animate-bounce" src="img/linkedin.png" alt="Linkedin Icon">
-                    </a></li>
-                    <li><a href="#">
-                        <img class="h-8 hover:animate-bounce" src="img/youtube.png" alt="Youtube Icon">
-                    </a></li>
-                </ul>
-                
-            </div>
-            <div class="w-1/3 flex items-center justify-center">
-                <div>
-                    <img class="h-40 -mt-2 hover:animate-bounce" src="img/avatar.svg" alt="Foto Vitor Renan">
-                </div>
-            </div>
-        </section>
+        <?php include('./componentes/hero.php'); ?>
+
         <section class="space-y-3 py-6" id="projetos">
-            <h2 class="text-2xl font-bold flex-col">
-                Meus projetos
-            </h2>
-           
-            <div class="bg-slate-800 rounded-lg p-3 flex items-center">
-                <div class= "w-1/5">Foto do projeto</div>
-                <div class="w-4/5 space-y-3">
-                    <div class="flex gap-3 justify-between">
-                        <h3 class="font-semibold text-xl">
-                            ✅ Projeto 1 <span class="text-xs text-gray-400 opacity-50 italic">(finalizado em 2024)</span>
-                        </h3>
-                        <div>
-                            <span class="bg-fuchsia-400 text-fuchsia-700 rounded-lg px-4 py-1 font-semibold text-xs">PHP</span>
-                            <span class="bg-lime-400 text-lime-900 rounded-lg px-4 py-1 font-semibold text-xs">HTML</span>
-                            <span class="bg-sky-400 text-sky-900 rounded-lg px-4 py-1 font-semibold text-xs">JAVASCRIPT</span>
-                            <span class="bg-rose-400 text-rose-900 rounded-lg px-4 py-1 font-semibold text-xs">CSS</span>
-                        </div>
-                    </div>
-                    <p class="leading-6">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus rerum nemo tempora voluptas sit repudiandae pariatur, aliquam autem eos excepturi quia facilis debitis magnam qui culpa aliquid quasi et enim cum non eum ullam deserunt iusto! Ratione debitis incidunt, nulla optio nobis fugit possimus magni quasi aliquam, placeat quidem architecto.
-                    </p>
-                </div>
-            </div>
+
+            <h2 class="text-2xl font-bold flex-col">Meus projetos</h2>
+            
+            <?php include('./componentes/projetos.php'); ?>
         </section>
     </main>
     <footer class="mx-auto max-w-screen-lg min-h-20">
         <div class="border-t border-gray-600 pt-6 px-3 text-gray-400 text-sm">
-            <p>© Copyright 2025. Construído por Vitor Renan de Almeida.</p>
+            <p>© Copyright <?=date('Y')?>. Construído por Vitor Renan de Almeida.</p>
         </div>
     </footer>
 </body>
