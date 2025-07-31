c5db5401d7ce204b3f9ae3e068611a974fdb517d
Vitor Renan <vitorrenanalmeida@gmail.com>
2025-07-31 13:22:22 -0300

    Daily Commit

--- Php/exercise002/livro.php
+++ Php/exercise002/livro.php
@@ -1,6 +1,19 @@
 <?php
 
-$id = $_REQUEST['id'];
+require 'dados.php';
+
+$id = (int) $_REQUEST['id'];
+
+$filtrado = array_filter($livros, fn($l) => $l['id'] === $id);
+
+$livro = array_pop($filtrado);
+
+// echo "<pre>";
+
+// var_dump($livro);
+
+// echo "</pre>";
+
 
 ?>
 
@@ -28,7 +41,22 @@ $id = $_REQUEST['id'];
         </nav>
     </header>
     <main class="mx-auto max-w-screen-lg space-y-6">
-        ID do livro: <?=$id?>
+        <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+                <div class="flex">
+                    <div class="w-1/3">
+                        imagem
+                    </div>
+                    <div class="space-y-1">
+                        <a href="/livro.php?id=<?=$livro['id']?>" class="font-semibold hover:underline"><?=$livro['titulo']?></a>
+                        <div class="text-xs italic"><?=$livro['autor']?></div>
+                        <div class="text-xs italic"> ⭐⭐⭐⭐⭐3 Avaliações</div>
+                    </div>
+                </div>
+            <div class="text-sm mt-2">
+                <?=$livro['descricao']?>
+            </div>
+        </div>
+        
     </main>
 </body>
 </html>
