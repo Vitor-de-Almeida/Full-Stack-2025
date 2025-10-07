4aacbe5c2028ccf9e9f9d65aa58d5c0dff46715d
Vitor Renan <vitorrenanalmeida@gmail.com>
2025-10-07 16:46:18 -0300

    Daily Commit

---
+++ fullStack/Exercises/002 - NodeJs/API/src/utils/database.js
@@ -0,0 +1,31 @@
+import fs from "node:fs/promises"
+const DATABASE_PATH = new URL ("db.json", import.meta.url)
+
+export class Database {
+    database = {}
+
+    constructor () { 
+        fs.readFile(DATABASE_PATH, "utf8")
+        .then((data) => {
+            this.database = JSON.parse(data)
+            console.log(this.database)
+        }).catch(() => this.persist())
+    }
+
+    persist () {
+        fs.writeFile(DATABASE_PATH, JSON.stringify(this.database, null, 2))
+    }
+
+    insert(table, data) {
+        if (Array.isArray(this.database[table])) {
+            this.database[table].push(data)
+        } else {
+            this.database[table] = [data]
+        }
+        this.persist()
+    }
+    select (table) {
+    return this.database[table]
+}
+}
+
