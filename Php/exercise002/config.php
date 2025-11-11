f5a00bcffc9829c4e506638ec9a4e2bfedc7598c
Vitor <vitorrenanalmeida@gmail.com>
2025-11-10 22:45:24 -0300

    database mysql

--- Php/exercise002/config.php
+++ Php/exercise002/config.php
@@ -1,8 +1,13 @@
 <?php
 return [
     'database' => [
-        'driver' => 'sqlite',
-        'database' => 'database.sqlite',
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'database' => 'bookwise',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
     ],
 ];
 ?>
