5b744f1a9da031efcd17a12aeab2aeeb2f95942f
Vitor <vitorrenanalmeida@gmail.com>
2025-11-14 17:14:59 -0300

    Shedule adjustment

--- fullStack/Project Hair Day/Hair Day/src/modules/form/submit.js
+++ fullStack/Project Hair Day/Hair Day/src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector('form');
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById('date');
 
 // Date atual para o input de data
@@ -15,4 +16,25 @@ selectedDate.min = inputToday
 form.onsubmit = (event) => {
     event.preventDefault();
     alert('Formulário enviado!');
+
+    try {
+        //Recuperando o nome do cliente
+        const name = clientName.value.trim();
+        if (!name) {
+            return alert("Por favor, insira seu nome")
+        }
+        const hourSelected = document.querySelector('.hour-selected');
+        console.log(hourSelected)
+        
+        if (!hourSelected) {
+            return alert("Por favor, selecione um horário")
+        }
+        const [hourNow] = hourSelected.innerText.split(";");
+
+        const when = dayjs(selectedDate.value).add(Number(hourNow), 'hour');
+        console.log(when)
+    } catch(error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 };
