7543204dd9b6b3c4589fd54c3386372d463a4e0c
Vitor <vitorrenanalmeida@gmail.com>
2026-01-23 16:11:09 -0300

    last daily commit

--- Php/LaravelNodestudioCourse/database/migrations/2026_01_23_180839_products.php
+++ Php/LaravelNodestudioCourse/database/migrations/2026_01_23_180839_products.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName');
-            $table->string('lastName');
+            $table->string('name');
+            $table->text('description');
+            $table->double('price', 8, 2);
+            $table-> 
             $table->timestamps();
         });
     }
