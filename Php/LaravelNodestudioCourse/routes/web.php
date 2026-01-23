0d77cb1284a0714ff42a1beebdd2c6b821a82b32
Vitor <vitorrenanalmeida@gmail.com>
2026-01-23 16:05:49 -0300

    Migration, DB and controller learning

--- Php/LaravelNodestudioCourse/routes/web.php
+++ Php/LaravelNodestudioCourse/routes/web.php
@@ -1,7 +1,59 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('products', ProductController::class);
+
+// 
+
+// Route::group([
+//     'prefix' => 'admin',
+//     'as' => 'admin.',
+// ], function() {
+    
+//     Route::get('dashboard', function() {
+//         return 'Dashboard';
+//     })->name('dashboard');
+
+//     Route::get('customers', function() {
+//         return 'Customers';
+//     })->name('customers');
+
+//     Route::get('users', function() {
+//         return 'Users';
+//     })->name('users');
+// });
+
+// Route::get('company', function() {
+//     return view('site/company');
+// });
+
+// route::view('/company','site/company');
+
+// Route::any('/any', function() {
+//     return 'Any Method';
+// });
+
+// Route::match(['put', 'post'], '/match', function() {
+//     return 'Match Method';
+// });
+
+// Route::get('/product/{id}/{cat}', function($id, $cat) {
+//     return "The Id product is: {$id} and the category is: {$cat}";
+// });
+
+// route::get('/about', function() {
+//     return redirect('/company');
+// });
+
+// Route::redirect('/about', '/company');
+
+// Route::get('/news', function() {
+//     return view('news');
+// })->name('newsindex');
+
+// Route::get('/latest', function() {
+//     return redirect()->route('newsindex');
+// });
+
