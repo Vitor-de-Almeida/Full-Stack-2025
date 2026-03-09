473c4e52b6a701cd4089fb059a303f8321637f95
Vitor <vitorrenanalmeida@gmail.com>
2026-03-09 13:39:41 -0300

    Editing route patch

--- fullStack/Exercises/003 - Tickets/src/controllers/updateStatus.js
+++ fullStack/Exercises/003 - Tickets/src/controllers/updateStatus.js
@@ -1,7 +1,10 @@
 export function updateStatus({request, response, database}) {
     const { id } = request.params
+    const { solution } = request.body
+
+    console.log(solution)
 
     database.update("tickets", id, { status: "closed" })
 
-    return response.writeHead(204).end()
+    return response.writeHead(200).end()
 }
