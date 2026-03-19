9c49c746c4f3673f8e2058c0e53133ca2657d459
Vitor <vitorrenanalmeida@gmail.com>
2026-03-19 16:19:19 -0300

    Making validations

--- fullStack/Exercises/004 - API REST/src/controllers/Product-Controllers.ts
+++ fullStack/Exercises/004 - API REST/src/controllers/Product-Controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { AppError } from "../utils/App-Error";
+import { z } from "zod";
 
 class ProductController {
 
@@ -9,10 +11,46 @@ class ProductController {
     }
 
     create (request: Request, response: Response) {
-        const {name, price, } = request.body;
+
+        const bodySchema = z.object({
+            name: z
+                .string({required_error:"Name is required"})
+                .trim()
+                .min(6, {message: "Name must be at least 6 characters long"}),
+            
+            price: z
+                .number({required_error:"Price is required"})
+                .min(1, {message: "Price must be greater than 0"})
+                .positive({message: "Price must be positive"})
+                .gte(10, {message: "Price must be greater than or equal to 10"}),
+        })
+
+        const { name, price } =bodySchema.parse(request.body);
+
         response.status(201).json({name, price, user_id: request.user_id});
 }
 
 }
 
 export { ProductController };
+
+
+ // if(!name) {
+        //     throw new AppError("Name is required");
+        // }
+
+        // if(!price) {
+        //     throw new AppError("Price is required");
+        // }
+
+        // if(!name && !price) {
+        //     throw new AppError("Name and price are required");
+        // }
+
+        // if(name.trim().length < 6) {
+        //     throw new AppError("Name must be at least 6 characters long");
+        // }
+
+        // if(price <= 0) {
+        //     throw new AppError("Price must be greater than 0");
+        // }
--- fullStack/Exercises/004 - API REST/src/server.ts
+++ fullStack/Exercises/004 - API REST/src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { Request, Response, NextFunction} from "express";
 import { routes } from "./routes";
+import { AppError } from "./utils/App-Error";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -8,6 +10,18 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error:any, request:Request, response: Response, next: NextFunction) => {
+    
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({message: error.message})
+    }
+    
+    if (error instanceof ZodError) {
+        return response.status(400).json({message: "Validation error: ", issues:error.format()})
+    }
+    return response.status(500).json({message: error.message})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
