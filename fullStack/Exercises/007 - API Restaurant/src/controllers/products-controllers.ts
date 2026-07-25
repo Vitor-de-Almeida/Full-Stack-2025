1533b317a435bdb898328b52d61ca8a74a8b5d9d
Vitor <vitorrenanalmeida@gmail.com>
2026-07-25 11:13:16 -0300

    Creating migrations and Database Sqlite

--- fullStack/Exercises/007 - API Restaurant/src/controllers/products-controllers.ts
+++ fullStack/Exercises/007 - API Restaurant/src/controllers/products-controllers.ts
@@ -1,14 +1,18 @@
 import { productsRoutes } from "@/routes/products-routes";
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
 
 class ProductController {
     async index(request: Request, response: Response, next: NextFunction) {
         try {
-
-            return response.json( { message: "Ok" })
+            
+            throw new AppError("Test", 400);
+            //return response.json( { message: "Ok" })
 
         } catch (error) {
+
             next(error);
+
         }
 
     }
