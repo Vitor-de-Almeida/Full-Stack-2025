3196ee4a64324bc42fdce53efee54e5c06eb4a29
Vitor Renan <vitorrenanalmeida@gmail.com>
2026-08-21 18:25:11 -0300

    Adjusted the app to user search something by a word and updated and delete the question

---
+++ fullStack/ORM/prisma/seed.ts
@@ -0,0 +1,25 @@
+import { prisma } from "../src/prisma";
+
+async function seed() {
+    await prisma.user.createMany({
+        data: [
+            {
+                name: "John Doe",
+                email: "john.doe@example.com",
+            },
+            {
+                name: "Jane Doe",
+                email: "jane.doe@example.com",
+            },
+            {
+                name: "John Smith",
+                email: "john.smith@example.com",
+            },
+        ]
+    })
+}
+
+seed().then(() => {
+    console.log("Database seeded successfully");
+    prisma.$disconnect();
+})
--- fullStack/ORM/src/controllers/questions-controller.ts
+++ fullStack/ORM/src/controllers/questions-controller.ts
@@ -3,7 +3,17 @@ import { prisma } from "../prisma"
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    const questions = await prisma.question.findMany({include: {user: true}})
+    const questions = await prisma.question.findMany({
+      where: {
+        title: {
+          contains: request.query.title?.toString().trim(),
+          mode: "insensitive",
+        },
+      },
+      orderBy: {
+        title: "asc",
+      }
+    })
 
     return response.json(questions)
   }
@@ -23,12 +33,26 @@ class QuestionsController {
   }
 
   async update(request: Request, response: Response) {
-    return response.json()
+    const { id } = request.params
+
+    const { title, content } = request.body
+    await prisma.question.update({
+
+      where: { id },
+
+      data: { title, content },
+
+    })
+    return response.json({ message: "Question updated successfully" })
   }
 
   async remove(request: Request, response: Response) {
-    return response.json()
+    const { id } = request.params
+
+    await prisma.question.delete({ where: { id } })
+    return response.json({ message: "Question deleted successfully" })
   }
+  
 }
 
 export { QuestionsController }
