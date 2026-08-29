5558b730a3bb94b07b4d27e99a433c6366cb0234
Vitor Renan <vitorrenanalmeida@gmail.com>
2026-08-29 20:51:27 -0300

    Best point to learn useEffect

--- fullStack/React/classRoom/src/App.tsx
+++ fullStack/React/classRoom/src/App.tsx
@@ -1,20 +1,34 @@
 import "./global.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Button } from "./components/button"
 import styles from "./app.module.css"
 //import { useMath } from "./hooks/useMessages"
 
 export function App() {
 
+
   const [count, setCount] = useState(0)
   //const { sum, sub, name } = useMath()
+
+  function handleAdd() {
+    setCount((prevCount) => prevCount+1)
+  }
+
+  function handleRemove() {
+    setCount((prevCount) => prevCount-1)
+  }
+
+  useEffect(() => {
+    console.log("count:", count)
+  }, []) //dependency array
+
   return (
     <div className={styles.container}>
-      <Button name="Add" onClick={()=>setCount(count+1)}/>
+      <Button name="Add" onClick={handleAdd}/>
 
       <span>{count}</span>
 
-      <Button name="Remove" onClick={()=>setCount(count-1)}/>
+      <Button name="Remove" onClick={handleRemove}/>
 
     </div>
   )
