cf15809cb74764405def31fdada6ac0713af8237
Vitor Renan <vitorrenanalmeida@gmail.com>
2026-08-29 11:59:36 -0300

    Making Components

--- fullStack/React/classRoom/src/App.tsx
+++ fullStack/React/classRoom/src/App.tsx
@@ -1,3 +1,11 @@
+import { Button } from "./components/button"
+
 export function App() {
-  return <div>Hellasdd!</div>
+  return (
+      <div>
+        <Button name="Create" onClick={() => alert("Create")}/>
+        <Button name="Salve" />
+        <Button name="Edit"/>
+      </div>
+  )
 }
